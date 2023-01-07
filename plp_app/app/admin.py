5f205bb66bf1fef53947352fd6c95c36e1542177
louykiller <lourencoccvc@gmail.com>
2023-01-07 17:41:43 +0000

    changed our user to django user

--- plp_app/app/admin.py
+++ plp_app/app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.User)
 admin.site.register(models.Admin)
 admin.site.register(models.Profile)
 admin.site.register(models.Public)
--- plp_app/app/models.py
+++ plp_app/app/models.py
@@ -1,16 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-
-
-class User (models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.username
-
-
 class Admin(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
 
