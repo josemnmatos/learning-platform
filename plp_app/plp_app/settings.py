66d4075af3648a5f37c785132cfe266b948f2e88
José Matos <83038703+josemnmatos@users.noreply.github.com>
2023-01-08 21:18:26 +0100

    render3

--- plp_app/plp_app/settings.py
+++ plp_app/plp_app/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -128,6 +130,7 @@ STATICFILES_FINDERS = (
 )
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
